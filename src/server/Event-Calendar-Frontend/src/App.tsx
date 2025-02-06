61729f3fb309f61dee7c44c24120da6002e727e2
AveryEMcG <avery.erwinmcguire@gmail.com>
2025-02-06 17:30:44 -0500

    storing weeks in state

--- src/server/Event-Calendar-Frontend/src/App.tsx
+++ src/server/Event-Calendar-Frontend/src/App.tsx
@@ -1,16 +1,35 @@
+import { useState } from 'react'
 import { Calendar, dateFnsLocalizer} from 'react-big-calendar'
 import format from 'date-fns/format'
 import parse from 'date-fns/parse'
 import startOfWeek from 'date-fns/startOfWeek'
 import getDay from 'date-fns/getDay'
 import enUS from 'date-fns/locale/en-US'
+import addHours from 'date-fns/addHours'
+
 
 import './App.css'
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-function App() {
+
+//grabs a series of the 0th hour from the past week. Necessary to pre-populate date range
+function initalizeDateRange(){
+  let curr = new Date 
+  let week = []
   
+  let day = startOfWeek(curr)
+  week.push(day)
+  for (let i = 0; i <= 6; i++) {
+    day = addHours(day,24)
+    week.push(day)
+  }
+  return week
+}
+
+function App() {
+const [dateRange, setDateRange]= useState<Date[]>(initalizeDateRange())
+
 const locales = {
   'en-US': enUS,
 }
@@ -21,10 +40,14 @@ const locales = {
     getDay,
     locales,
   })
+
+  const onRangeChange = data => {setDateRange(data)}
+
   return (
     <Calendar
       defaultView='week'
       events={[]}
+      onRangeChange={onRangeChange}
       localizer={localizer}
       style={{ height: '100vh' }}
     />
