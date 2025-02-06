c56a613c843ec195fcbb6b4aa8af2bedd9982c6c
AveryEMcG <avery.erwinmcguire@gmail.com>
2025-02-06 17:15:12 -0500

    adding empty calendar

--- src/server/Event-Calendar-Frontend/src/App.tsx
+++ src/server/Event-Calendar-Frontend/src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Calendar, dateFnsLocalizer} from 'react-big-calendar'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import enUS from 'date-fns/locale/en-US'
+
 import './App.css'
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+const locales = {
+  'en-US': enUS,
+}
+  const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+  })
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Calendar
+      defaultView='week'
+      events={[]}
+      localizer={localizer}
+      style={{ height: '100vh' }}
+    />
   )
 }
 
