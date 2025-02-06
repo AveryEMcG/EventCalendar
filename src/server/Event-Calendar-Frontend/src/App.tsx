fd08ee52fb606600c2225edae04809e084b87b4e
AveryEMcG <avery.erwinmcguire@gmail.com>
2025-02-06 17:36:32 -0500

    Update App.tsx

--- src/server/Event-Calendar-Frontend/src/App.tsx
+++ src/server/Event-Calendar-Frontend/src/App.tsx
@@ -13,6 +13,30 @@ import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 
+// This is our database representation of scheduled events, 
+// we'll need to transform it to be used by react-big-calendar
+interface ScheduledEvent {
+  name: string;
+  id: number;
+  start_time: Date;
+  duration: number;
+  repeats_m: boolean;
+  repeats_t: boolean;
+  repeats_w: boolean;
+  repeats_th: boolean; 
+  repeats_f: boolean;    
+  repeats_s: boolean;  
+  repeats_su: boolean;  
+}
+
+// This is the react-big-calendar event
+interface Event {
+  name: string;
+  id: number;
+  start: Date;
+  end: Date;
+}
+
 //grabs a series of the 0th hour from the past week. Necessary to pre-populate date range
 function initalizeDateRange(){
   let curr = new Date 
@@ -28,8 +52,10 @@ function initalizeDateRange(){
 }
 
 function App() {
+const [events, setEvents] = useState<ScheduledEvent[]>([])
 const [dateRange, setDateRange]= useState<Date[]>(initalizeDateRange())
 
+// necessary init data for proper calendar localization
 const locales = {
   'en-US': enUS,
 }
@@ -41,6 +67,7 @@ const locales = {
     locales,
   })
 
+  // when we click through the weeks in the calendar, update our internal state
   const onRangeChange = data => {setDateRange(data)}
 
   return (
