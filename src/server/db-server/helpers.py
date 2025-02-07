1279725bc51f95c8c41ff8ff0aa9d64ee536033b
AveryEMcG <avery.erwinmcguire@gmail.com>
2025-02-07 16:38:46 -0500

    Correcting overlap logic

--- src/server/db-server/helpers.py
+++ src/server/db-server/helpers.py
@@ -2,92 +2,43 @@ import db
 import datetime
 
 SECONDS_IN_MINUTE = 60
+SECONDS_IN_DAY = 86400
+
+#we're also going to use some offsets
+SUNDAY_SECONDS = 0 * SECONDS_IN_DAY
+MONDAY_SECONDS = 1 * SECONDS_IN_DAY
+TUESDAY_SECONDS = 2 * SECONDS_IN_DAY
+WEDNESDAY_SECONDS = 3 * SECONDS_IN_DAY
+THURSDAY_SECONDS = 4 * SECONDS_IN_DAY
+FRIDAY_SECONDS = 5 * SECONDS_IN_DAY
+SATURDAY_SECONDS = 6 * SECONDS_IN_DAY
+
+# python does weekdays starting on Monday
+WEEK_SECONDS = [MONDAY_SECONDS,TUESDAY_SECONDS,WEDNESDAY_SECONDS,THURSDAY_SECONDS,FRIDAY_SECONDS,SATURDAY_SECONDS,SUNDAY_SECONDS]
 
 def isOvernight(start,end):
     if end<start:
         return True
     return False
+ 
+def isRepeating(event):
+    if event.repeats_su:
+        return True    
+    if event.repeats_m:
+        return True
+    if event.repeats_t:
+        return True
+    if event.repeats_w:
+        return True   
+    if event.repeats_th:
+        return True
+    if event.repeats_f:
+        return True
+    if event.repeats_s:
+        return True    
+    return False
 
-
-
-def timeIsUnique(newEvent):
-    eventList = db.query("SELECT * FROM ScheduledEvents")
-    newEventStartTime = datetime.datetime.fromtimestamp(newEvent["start_time"])
-    newEventEndTime = datetime.datetime.fromtimestamp((newEvent["start_time"])+(newEvent["duration"]*SECONDS_IN_MINUTE))
-
-    for e in eventList:
-
-        eventStartTime = datetime.datetime.fromtimestamp(e.start_time)
-        eventEndTime = datetime.datetime.fromtimestamp((e.start_time+e.duration*SECONDS_IN_MINUTE))
-        
-        #check if the absolute time is colliding
-        if isColliding(newEventStartTime,newEventEndTime,eventStartTime,eventEndTime):
-
-            return False
-        print("Passed Check 1, going to check 2")
-
-        # Now we'll check for repeating events
-        # first, are they occurring at the same time of day?
-        agnosticNewEventStart = getDayAgnosticMS(newEventStartTime)
-        agnosticNewEventEnd = getDayAgnosticMS(newEventEndTime)
-        agnosticExistingEventStart = getDayAgnosticMS(eventStartTime)
-        agnosticExistingEventEnd = getDayAgnosticMS(eventEndTime)
-
-        if currentlyCollides(agnosticNewEventStart,agnosticNewEventEnd,agnosticExistingEventStart,agnosticExistingEventEnd):
-
-
-            #if they're at the same time of day, they risk overlapping for day repeats.
-            #let's check! :)
-
-            #We need to check if they repeat on the same day, OR if there's a repeat
-            if (newEvent["repeats_su"] ==1):
-                if (e.repeats_su ==1):
-                    return False
-                if (eventStartTime.weekday() ==0):
-                    return False
-
-            if (newEvent["repeats_m"] ==1): 
-                if(e.repeats_m ==1):
-                    return False
-                if (eventStartTime.weekday() ==1):
-                    return False
-            
-            if (newEvent["repeats_t"] ==1):
-                if (e.repeats_t ==1):
-                    return False
-                if (eventStartTime.weekday() ==2):
-                    return False
-            
-            if (newEvent["repeats_w"] ==1):
-                if(e.repeats_w ==1):
-                    return False
-                if(eventStartTime.weekday() ==3):
-                    return False
-                        
-            if (newEvent["repeats_th"] ==1):
-                if(e.repeats_th ==1):
-                    return False
-                if(eventStartTime.weekday() ==4):
-                    return False
-            
-            if (newEvent["repeats_f"] ==1):
-                if(e.repeats_f ==1):
-                    return False
-                if(eventStartTime.weekday() ==5):
-                    return False        
-                                
-            if (newEvent["repeats_s"] ==1):
-                if(e.repeats_s ==1):
-                    return False
-                if(eventStartTime.weekday() ==6):
-                    return False     
-                                         
-    #if it didn't happen yet, we're good :)
-    print ("not colliding at all!")
-    return True
-    
-def currentlyCollides(start1,end1, start2,end2):
-
+def collides(start1,end1, start2,end2):
     first = None
     second = None
     
@@ -109,20 +60,85 @@ def currentlyCollides(start1,end1, start2,end2):
 
     return False
 
+
+def generateWeekProjection(event, absoluteStart, absoluteEnd):
+    projections = []  
+    print("generating projections for ", absoluteStart," on day ", datetime.datetime.fromtimestamp(event.start_time).weekday())
+    if (event.repeats_su ==1):
+                projections.append((absoluteStart+SUNDAY_SECONDS,absoluteEnd+SUNDAY_SECONDS))
+    if (event.repeats_m  ==1):
+                projections.append((absoluteStart+MONDAY_SECONDS,absoluteEnd+MONDAY_SECONDS))
+    if (event.repeats_t ==1):
+                projections.append((absoluteStart+TUESDAY_SECONDS,absoluteEnd+TUESDAY_SECONDS))           
+    if (event.repeats_w ==1):
+                projections.append((absoluteStart+WEDNESDAY_SECONDS,absoluteEnd+WEDNESDAY_SECONDS)) 
+    if (event.repeats_th ==1):
+                projections.append((absoluteStart+THURSDAY_SECONDS,absoluteEnd+THURSDAY_SECONDS))
+                print("Getting a thursday projection")           
+    if (event.repeats_f ==1):
+                 projections.append((absoluteStart+FRIDAY_SECONDS,absoluteEnd+FRIDAY_SECONDS))                       
+    if (event.repeats_s ==1):
+                projections.append((absoluteStart+SATURDAY_SECONDS,absoluteEnd+SATURDAY_SECONDS))  
+
+    print(projections)
+    return projections
+
+
+
+def timeIsUnique(newEvent,eventList):
+    newEventStartTime = datetime.datetime.fromtimestamp(newEvent.start_time)
+    newEventEndTime = datetime.datetime.fromtimestamp((newEvent.start_time)+(newEvent.duration*SECONDS_IN_MINUTE))
+
+    for existingEvent in eventList:
+
+        existingEventStartTime = datetime.datetime.fromtimestamp(existingEvent.start_time)
+        existingEventEndTime = datetime.datetime.fromtimestamp((existingEvent.start_time+existingEvent.duration*SECONDS_IN_MINUTE))
+        
+        # See if the event conflicts with other events
+        if collides(newEventStartTime,newEventEndTime,existingEventStartTime,existingEventEndTime):
+            return False
+        
+        # If it doesn't, we'll need to compare against repeating days
+        # to do that, we're going to make a 'projection':
+        # 1 - strip the day from the date (get the 'absolute' times)
+        # 2 - find out the days that will be repeated
+        # 3 - add the absolute time to an offset to form a projection
+        # 4 - append the projection to a list
+
+        # we will then see if the projections from either the original or new event might clash
+
+        # 1 - get the absolute times
+        newEventAbsoluteStart = getDayAgnosticMS(newEventStartTime)
+        newEventAbsoluteEnd = newEventAbsoluteStart+(newEvent.duration*SECONDS_IN_MINUTE)
+
+        existingEventAbsoluteStart = getDayAgnosticMS(existingEventStartTime)
+        existingEventAbsoluteEnd = existingEventAbsoluteStart+(existingEvent.duration*SECONDS_IN_MINUTE)
+        
+        
+        # 2-4 - make our projections 
+        newEventProjections = generateWeekProjection(newEvent,newEventAbsoluteStart,newEventAbsoluteEnd)
+        existingEventsProjections = generateWeekProjection(existingEvent,existingEventAbsoluteStart,existingEventAbsoluteEnd)
+
+        tempstart = newEventAbsoluteStart+WEEK_SECONDS[newEventStartTime.weekday()]
+        print("new event is happening on ", newEventStartTime.weekday(), "which has start time of", tempstart, "which is this % of a day: ",tempstart/SECONDS_IN_DAY )
+ 
+        tempstart = existingEventAbsoluteStart+WEEK_SECONDS[existingEventStartTime.weekday()]
+        print("old event is happening on ", existingEventStartTime.weekday(), "which has start time of", tempstart, "which is this % of a day: ",tempstart/SECONDS_IN_DAY )
+        #we're going to insert the new event to the projection as well
+        newEventProjections.append((newEventAbsoluteStart+WEEK_SECONDS[newEventStartTime.weekday()],newEventAbsoluteEnd+WEEK_SECONDS[newEventStartTime.weekday()]))
+        existingEventsProjections.append((existingEventAbsoluteStart+WEEK_SECONDS[existingEventStartTime.weekday()],existingEventAbsoluteEnd+WEEK_SECONDS[existingEventStartTime.weekday()]))
+
+
+        for projection1 in newEventProjections:
+            for projection2 in existingEventsProjections:
+                    print(projection1, " from the new projection")
+                    print(projection2, " from the existing projection")
+
+                    if collides(projection1[0],projection1[1],projection2[0],projection2[1]):
+                          return False
+    return True
+    
+
+
 def getDayAgnosticMS(date):
-    return date.hour*1000*60*60+(date.minute*1000*60)+(date.second*1000)
-
-
-
-
-def CurrentlyCollides(event1Start,event1End, event2Start,event2End):
-    return False
-
-def newEventRepeatsCollidesOnExistingEvent(event1Start,event1End, event1DayOfWeek, event2Start,event2End,event2DayOfWeek):
-    return False
-
-def newEventCollidesOnExistingEventRepeats(event1Start,event1End, event1DayOfWeek, event2Start,event2End,event2DayOfWeek):
-    return False
-
-def newEventRepeatsCollidesOnExistingEventRepeats(event1Start,event1End, event1DayOfWeek, event2Start,event2End,event2DayOfWeek):
-    return False
+    return date.hour*60*60+(date.minute*60)+(date.second)
--- src/server/db-server/main.py
+++ src/server/db-server/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
 
 #{"body":{"name":"n/a","id":0,"duration":100,"start_time":1739031060,"repeats_su":false,"repeats_m":false,"repeats_t":false,"repeats_w":false,"repeats_th":false,"repeats_f":false,"repeats_s":false}}
 
-class Event(BaseModel):
+class Event_Struct(BaseModel):
     name: str
     start_time: int
     duration: int
@@ -32,7 +32,10 @@ class Event(BaseModel):
     repeats_s: int
 
 class Body(BaseModel):
-    body: Event
+    body: Event_Struct
+
+def eventStructToDBEvent(newEvent):
+      return db.Event(None, newEvent["name"],newEvent["start_time"],newEvent["duration"],newEvent["repeats_su"],newEvent["repeats_m"],newEvent["repeats_t"],newEvent["repeats_w"],newEvent["repeats_th"],newEvent["repeats_f"],newEvent["repeats_s"])
 
 
 @app.get("/events")
@@ -44,7 +47,9 @@ def get_events():
 @app.post("/events")
 async def create_event(event: Body):
         event_dict = event.body.model_dump()
-        if(helpers.timeIsUnique(event_dict)):
+        newDbEvent = eventStructToDBEvent(event_dict)
+        eventList = db.query("SELECT * FROM ScheduledEvents")
+        if(helpers.timeIsUnique(newDbEvent,eventList)):
                 result = db.insert("INSERT INTO ScheduledEvents VALUES (NULL,\""+str(event_dict["name"])+"\","+str(event_dict["start_time"])+","+str(event_dict["duration"])+","+str(event_dict["repeats_su"])+","+str(event_dict["repeats_m"])+","+str(event_dict["repeats_t"])+","+str(event_dict["repeats_w"])+","+str(event_dict["repeats_th"])+","+str(event_dict["repeats_f"])+","+str(event_dict["repeats_s"])+")")
         else:
                 raise HTTPException(status_code=400, detail="An event already exists at the time specified")
