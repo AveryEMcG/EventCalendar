a2f8a47758c1ff0e76964a8ae73fdd74af46341e
AveryEMcG <avery.erwinmcguire@gmail.com>
2025-02-07 20:41:37 -0500

    updates

--- src/server/db-server/helpers.py
+++ src/server/db-server/helpers.py
@@ -1,17 +1,19 @@
 import db
 import datetime
 
+
+# Defining some times here, we'll use them in our calculations below:
 SECONDS_IN_MINUTE = 60
 SECONDS_IN_DAY = 86400
 
-# we're also going to use some offsets
-SUNDAY_SECONDS = 0 * SECONDS_IN_DAY
-MONDAY_SECONDS = 1 * SECONDS_IN_DAY
-TUESDAY_SECONDS = 2 * SECONDS_IN_DAY
-WEDNESDAY_SECONDS = 3 * SECONDS_IN_DAY
-THURSDAY_SECONDS = 4 * SECONDS_IN_DAY
-FRIDAY_SECONDS = 5 * SECONDS_IN_DAY
-SATURDAY_SECONDS = 6 * SECONDS_IN_DAY
+# we're also going to use some offsets into a week
+MONDAY_SECONDS = 0 * SECONDS_IN_DAY
+TUESDAY_SECONDS = 1 * SECONDS_IN_DAY
+WEDNESDAY_SECONDS = 2 * SECONDS_IN_DAY
+THURSDAY_SECONDS = 3 * SECONDS_IN_DAY
+FRIDAY_SECONDS = 4 * SECONDS_IN_DAY
+SATURDAY_SECONDS = 5 * SECONDS_IN_DAY
+SUNDAY_SECONDS = 6 * SECONDS_IN_DAY
 
 # python does weekdays starting on Monday
 WEEK_SECONDS = [
@@ -24,13 +26,7 @@ WEEK_SECONDS = [
     SUNDAY_SECONDS,
 ]
 
-
-def isOvernight(start, end):
-    if end < start:
-        return True
-    return False
-
-
+#Check if an event is repeating
 def isRepeating(event):
     if event.repeats_su:
         return True
@@ -49,6 +45,7 @@ def isRepeating(event):
     return False
 
 
+# Check if two events are intersecting
 def collides(start1, end1, start2, end2):
     first = None
     second = None
@@ -68,10 +65,75 @@ def collides(start1, end1, start2, end2):
     # if the first event ends before the second one starts, there's collision
     if first[1] > second[0]:
         return True
-
     return False
 
 
+# events have duration and start time, this makes getting the end time easier in-line
+def getEndTimeForEvent(event):
+    return (event.start_time) + (event.duration * SECONDS_IN_MINUTE)
+
+# Check if there's collisions for the first event (sans its repeats)
+def firstWeekCollides(event,newEvent):
+    #project out a week from the exisiting event
+    week = generateFirstWeekProjection(event, event.start_time, getEndTimeForEvent(event))
+    for w in week:
+        # if that repeating event is AFTER or INTERSECTS that event, double check whether it collides
+        if (newEvent.start_time >= w[1]):
+            if collides(w[0], w[1], newEvent.start_time, getEndTimeForEvent(newEvent)):
+                return True
+    return False
+
+# Find out how many seconds from a date until a day of the week. This is NOT a good way to solve this.
+def addUntilDay(date, day):
+    days = 0
+    while( date.weekday()+days*SECONDS_IN_DAY < day):
+        days+1
+    return days*SECONDS_IN_DAY
+    
+
+# Takes a event, and returns a list of subsequent start/stop times for 1 week AFTER the initial event
+def generateFirstWeekProjection(event, start, end):
+    startDate = datetime.datetime.fromtimestamp(start)
+    projections = []
+    if event.repeats_su == 1:
+        offset = addUntilDay(startDate,6)
+        projections.append(
+            (start + offset, end + offset)
+        )
+    if event.repeats_m == 1:
+        offset = addUntilDay(startDate,0)
+        projections.append(
+            (start + offset, end + offset)
+        )
+    if event.repeats_t == 1:
+        offset = addUntilDay(startDate,1)
+        projections.append(
+            (start + offset, end + offset)
+        )
+    if event.repeats_w == 1:
+        offset = addUntilDay(startDate,2)
+        projections.append(
+            (start + offset, end + offset)
+        )
+    if event.repeats_th == 1:
+        offset = addUntilDay(startDate,3)
+        projections.append(
+            (start + offset, end + offset)
+        )
+    if event.repeats_f == 1:
+        offset = addUntilDay(startDate,4)
+        projections.append(
+            (start + offset, end + offset)
+        )
+    if event.repeats_s == 1:
+        offset = addUntilDay(startDate,5)
+        projections.append(
+            (start + offset, end + offset)
+        )
+    return projections
+
+
+# Takes a event, and returns a list of start/stop times for 1 week 
 def generateWeekProjection(event, absoluteStart, absoluteEnd):
     projections = []
     if event.repeats_su == 1:
@@ -106,21 +168,26 @@ def generateWeekProjection(event, absoluteStart, absoluteEnd):
     return projections
 
 
+
+#Checks if time is unique. Relies on the functions above!
 def timeIsUnique(newEvent, eventList):
+
+    #prepare our new event
     newEventStartTime = datetime.datetime.fromtimestamp(newEvent.start_time)
     newEventEndTime = datetime.datetime.fromtimestamp(
-        (newEvent.start_time) + (newEvent.duration * SECONDS_IN_MINUTE)
+        getEndTimeForEvent(newEvent)
     )
 
+    # look at prior existing events
     for existingEvent in eventList:
         existingEventStartTime = datetime.datetime.fromtimestamp(
             existingEvent.start_time
         )
         existingEventEndTime = datetime.datetime.fromtimestamp(
-            (existingEvent.start_time + existingEvent.duration * SECONDS_IN_MINUTE)
+                  getEndTimeForEvent(existingEvent)
         )
 
-        # See if the event conflicts with other events
+        # See if the event conflicts with the other event (sans repeats)
         if collides(
             newEventStartTime,
             newEventEndTime,
@@ -129,7 +196,10 @@ def timeIsUnique(newEvent, eventList):
         ):
             return False
 
-        # 1 - get the absolute times (which is a second offset from midnight on a given day)
+        # Check if there is a collision for the initial week
+        if firstWeekCollides(newEvent,existingEvent):
+            return False
+       
         newEventAbsoluteStart = getDayAgnosticMS(newEventStartTime)
         newEventAbsoluteEnd = newEventAbsoluteStart + (
             newEvent.duration * SECONDS_IN_MINUTE
@@ -146,22 +216,6 @@ def timeIsUnique(newEvent, eventList):
         )
         existingEventsProjections = generateWeekProjection(
             existingEvent, existingEventAbsoluteStart, existingEventAbsoluteEnd
-        )
-
-        # we're going to insert the new event to the projection as well
-        newEventProjections.append(
-            (
-                newEventAbsoluteStart + WEEK_SECONDS[newEventStartTime.weekday()],
-                newEventAbsoluteEnd + WEEK_SECONDS[newEventStartTime.weekday()],
-            )
-        )
-        existingEventsProjections.append(
-            (
-                existingEventAbsoluteStart
-                + WEEK_SECONDS[existingEventStartTime.weekday()],
-                existingEventAbsoluteEnd
-                + WEEK_SECONDS[existingEventStartTime.weekday()],
-            )
         )
 
         for projection1 in newEventProjections:
--- src/server/db-server/test.py
+++ src/server/db-server/test.py
@@ -2,165 +2,219 @@ import unittest
 import helpers
 import db
 
+# The only testing so far is for the timeIsUnique function (because it's the most complicated)
 
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        # Thursday, February 6, 2025 11:00:00 AM GMT-05:00
-        thursdayMorning = 1738857600
+wednesdayMorning = 1738771200  # Wednesday, February 5, 2025 11:00:00 AM GMT-05:00
+wednesdayNight = 1738816200  # Wednesday, February 5, 2025 11:30:00 PM GMT-05:00
+thursdayMorning = 1738857600  # Thursday, February 6, 2025 11:00:00 AM GMT-05:00
+
+
+class TestTimeIsUnique(unittest.TestCase):
+    def test_overlapping_events(self):
         data = db.Event(
-            None,
-            "test",
-            thursdayMorning,
-            60,
-            False,
-            False,
-            False,
-            False,
-            False,
-            False,
-            False,
+            id=None,
+            name="Thursday Morning Event1",
+            start_time=thursdayMorning,
+            duration=60,
+            repeats_su=False,
+            repeats_m=False,
+            repeats_t=False,
+            repeats_w=False,
+            repeats_th=False,
+            repeats_f=False,
+            repeats_s=False,
         )
         eventList = [
             db.Event(
-                None,
-                "test",
-                thursdayMorning,
-                60,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
+                id=None,
+                name="Thursday Morning Event2",
+                start_time=thursdayMorning,
+                duration=60,
+                repeats_su=False,
+                repeats_m=False,
+                repeats_t=False,
+                repeats_w=False,
+                repeats_th=False,
+                repeats_f=False,
+                repeats_s=False,
             )
         ]
+
+        # events scheduled at same time should fail
         self.assertFalse(helpers.timeIsUnique(data, eventList))
 
-        # Wednesday, February 5, 2025 11:00:00 AM GMT-05:00
-        wednesdayMorning = 1738771200
+    def test_event_overlaps_repeats(self):
         data = db.Event(
-            None,
-            "Wednesday Morning Repeating",
-            wednesdayMorning,
-            60,
-            False,
-            False,
-            False,
-            False,
-            True,
-            False,
-            False,
+            id=None,
+            name="Thursday Morning Repeating",
+            start_time=thursdayMorning,
+            duration=60,
+            repeats_su=False,
+            repeats_m=False,
+            repeats_t=False,
+            repeats_w=False,
+            repeats_th=True,
+            repeats_f=False,
+            repeats_s=False,
         )
         eventList = [
             db.Event(
-                None,
-                "test",
-                thursdayMorning,
-                60,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
+                id=None,
+                name="Wednesday but Repeating on Thursday Mornings",
+                start_time=wednesdayMorning,
+                duration=60,
+                repeats_su=False,
+                repeats_m=False,
+                repeats_t=False,
+                repeats_w=False,
+                repeats_th=True,
+                repeats_f=False,
+                repeats_s=False,
             )
         ]
+        # an event scheduled to repeat onto another event should fail
         self.assertFalse(helpers.timeIsUnique(data, eventList))
 
-        # Wednesday, February 5, 2025 11:00:00 AM GMT-05:00
-        wednesdayMorning = 1738771200
+    def test_repeats_overlap(self):
         data = db.Event(
-            None,
-            "Wednesday Morning Repeating",
-            wednesdayMorning,
-            60,
-            False,
-            False,
-            False,
-            False,
-            False,
-            False,
-            True,
+            id=None,
+            name="Wednesday Morning Repeating Saturday",
+            start_time=wednesdayMorning,
+            duration=60,
+            repeats_su=False,
+            repeats_m=False,
+            repeats_t=False,
+            repeats_w=False,
+            repeats_th=False,
+            repeats_f=False,
+            repeats_s=True,
         )
         eventList = [
             db.Event(
-                None,
-                "test",
-                thursdayMorning,
-                60,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                True,
+                id=None,
+                name="Thursday Morning repeating Saturday",
+                start_time=thursdayMorning,
+                duration=60,
+                repeats_su=False,
+                repeats_m=False,
+                repeats_t=False,
+                repeats_w=False,
+                repeats_th=True,
+                repeats_f=False,
+                repeats_s=True,
             )
         ]
+        # events scheduled to repeat over another repeat should fail
         self.assertFalse(helpers.timeIsUnique(data, eventList))
 
-        # Wednesday, February 5, 2025 11:30:00 PM GMT-05:00
-        wednesdayNight = 1738816200
+    def test_repeats_overlap(self):
         data = db.Event(
-            None,
-            "Wednesday night full day",
-            wednesdayNight,
-            24 * 60,
-            False,
-            False,
-            False,
-            False,
-            False,
-            False,
-            False,
+            id=None,
+            name="Wednesday night",
+            start_time=wednesdayNight,
+            duration=60,
+            repeats_su=False,
+            repeats_m=True,
+            repeats_t=False,
+            repeats_w=False,
+            repeats_th=False,
+            repeats_f=False,
+            repeats_s=False,
         )
         eventList = [
             db.Event(
-                None,
-                "test",
-                thursdayMorning,
-                60,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
+                id=None,
+                name="Wednesday Morning",
+                start_time=wednesdayMorning,
+                duration=60,
+                repeats_su=False,
+                repeats_m=False,
+                repeats_t=False,
+                repeats_w=False,
+                repeats_th=False,
+                repeats_f=False,
+                repeats_s=False,
+            ),
+            db.Event(
+                id=None,
+                name="Thursday Morning",
+                start_time=thursdayMorning,
+                duration=60,
+                repeats_su=False,
+                repeats_m=False,
+                repeats_t=False,
+                repeats_w=False,
+                repeats_th=False,
+                repeats_f=False,
+                repeats_s=True,
+            ),
+        ]
+        # This should work even with many events already exisiting
+        self.assertTrue(helpers.timeIsUnique(data, eventList))
+
+    def test_overnight(self):
+        data = db.Event(
+            id=None,
+            name="Wednesday night full day",
+            start_time=wednesdayNight,
+            duration=24 * 60,
+            repeats_su=False,
+            repeats_m=False,
+            repeats_t=False,
+            repeats_w=False,
+            repeats_th=True,
+            repeats_f=False,
+            repeats_s=True,
+        )
+        eventList = [
+            db.Event(
+                id=None,
+                name="Thursday Morning",
+                start_time=thursdayMorning,
+                duration=60,
+                repeats_su=False,
+                repeats_m=False,
+                repeats_t=False,
+                repeats_w=False,
+                repeats_th=False,
+                repeats_f=False,
+                repeats_s=False,
             )
         ]
+        # we should find clashes across day boundaries
         self.assertFalse(helpers.timeIsUnique(data, eventList))
 
+    def test_before_repeats(self):
         data = db.Event(
-            None,
-            "Wednesday night full day",
-            wednesdayNight,
-            24 * 60,
-            False,
-            False,
-            False,
-            False,
-            False,
-            False,
-            False,
+            id=None,
+            name="Wednesday Morning",
+            start_time=wednesdayMorning,
+            duration=60,
+            repeats_su=False,
+            repeats_m=False,
+            repeats_t=False,
+            repeats_w=False,
+            repeats_th=False,
+            repeats_f=False,
+            repeats_s=False,
         )
         eventList = [
             db.Event(
-                None,
-                "test",
-                wednesdayMorning,
-                60,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
+                id=None,
+                name="Thursday Morning",
+                start_time=thursdayMorning,
+                duration=60,
+                repeats_su=False,
+                repeats_m=False,
+                repeats_t=False,
+                repeats_w=True,
+                repeats_th=False,
+                repeats_f=False,
+                repeats_s=False,
             )
         ]
+        # If you schedule an event which IF it repeated, would clash,
+        # but it's happening BEFORE that repeating event and does not repeat on that day, it should pass
         self.assertTrue(helpers.timeIsUnique(data, eventList))
 
 
